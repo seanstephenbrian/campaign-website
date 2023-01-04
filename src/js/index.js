21f841c28aa2bef0b956b81ca89a263bad30eacb
sean stephen brian <seanstephenbrian@gmail.com>
2023-01-04 11:54:55 -0500

    create & style footer, adjust nav displacement

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,16 @@
 import '../style.css';
 import htmlBody from '../html/index.html';
 
-const body = document.querySelector('body');
-body.innerHTML = htmlBody;
+(function renderBody() {
+    const body = document.querySelector('body');
+    body.innerHTML = htmlBody;
+})();
+
+(function fillFooter() {
+    const footer = document.querySelector('footer');
+    const date = new Date();
+    footer.innerHTML = `
+        <p>© ${date.getFullYear()} John Martin for District 65 Board of Education</p>
+        <p>All rights reserved.</p>
+    `;
+})();
