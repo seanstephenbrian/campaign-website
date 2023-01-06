5d24110ed168dcb05c85e943f03b2acf75c72e27
sean stephen brian <seanstephenbrian@gmail.com>
2023-01-06 17:34:51 -0500

    ensure noopener for facebook js link

--- src/js/fb.js
+++ src/js/fb.js
@@ -1,6 +1,8 @@
 const sendToFacebook = () => {
     const newWindow = window.open('https://facebook.com/100088939831896/', '_blank', 'noopener');
-    newWindow.opener = null;
+    if (newWindow) {
+        newWindow.opener = null;
+    }
 }
 
 export default sendToFacebook;
