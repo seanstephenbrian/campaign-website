6380d5a188a6cbbdc571b90e661ab96b4c26d447
sean stephen brian <seanstephenbrian@gmail.com>
2023-01-06 10:58:12 -0500

    clean up page-state.js

--- src/js/page-state.js
+++ src/js/page-state.js
@@ -1,18 +1,13 @@
 const updatePageState = (page, clickedBack) => {
     const url = new URL(window.location);
+
     // update the search params:
-            // const params = new URLSearchParams(location.search);
-            // params.set('pg', page);
     url.searchParams.set('pg', page);
-    console.log(url.searchParams.get('pg'));
+    
     // if the user DID NOT click back, push the new URL to the browser history stack:
     if (clickedBack !== true) {
         console.log('did not click back');
         window.history.pushState({}, '', url);
-    } else if (clickedBack === undefined) {
-        console.log('clicked back');
-        // if the user DID click back, replace the history state with the new (previously visited) URL:
-        // window.history.replaceState({}, '', url);
     }
 }
 
