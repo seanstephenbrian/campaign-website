1ae0f32db9b45c98c441a7efa01b2d57284981c5
sean stephen brian <seanstephenbrian@gmail.com>
2023-02-14 09:11:03 -0500

    fill out supporters page content
    the supporters list is generated from an array of current supporters in
    the supporters.js render file

--- src/js/supporters.js
+++ src/js/supporters.js
@@ -12,6 +12,66 @@ const renderSupporters = (clickedBack) => {
     prevMain.remove();
     main.innerHTML = supportersMain;
     main.className = 'supporters-main';
+
+    // array of current supporters:
+    const currentSupporters = [
+        "Kristen & Ethan Baron",
+        "Svetlana Bekman",
+        "Ira & Debrah Berger",
+        "Michael Bess",
+        "Angela Blaising",
+        "Sydney Blattner",
+        "Amy Bolinder",
+        "Mike & Stephanie Boll",
+        "Angelique Cullen-Ketzback",
+        "Timothy Demas ",
+        "Dana Dodzik",
+        "Sharon Doyle",
+        "Barry Doyle",
+        "Sunil D’Souza",
+        "Scott & Diane Dugan",
+        "Roger Goldberg",
+        "Charlotte Grill",
+        "Kristin Kutzner Huzar",
+        "Evan Jacover",
+        "Alex & Lauren Janus",
+        "Dan G. Javor",
+        "Don Johnson",
+        "Peter & Jessica Kaplan",
+        "Susan B. Kaplan",
+        "Lori & Peter Keenan",
+        "Eric Krzysyofiak",
+        "Pam Lagger",
+        "Mike & Marie Levy",
+        "Kevin D. Lubawski",
+        "Terri Lydon",
+        "Sarah & Kevin Malia",
+        "James Martin",
+        "Cecile McHugh",
+        "Kate Monte",
+        "Jill Zordan & Eddie Moulds",
+        "Michael Murawczyk",
+        "Elena Nocera",
+        "Cindy Paulauskas",
+        "Julie & Eric Rosenberg",
+        "Susan Sevcik",
+        "Michael Simon",
+        "Tom & Lisa Stanton",
+        "Spencer Stern",
+        "Thomas M. Suffredin",
+        "Liz Tisdahl",
+        "William Wesenberg",
+        "Ray & Mary Anne Wexler"
+    ];
+
+    // method of rendering this array to the page:
+    const listContainer = document.querySelector('.supporters-list');
+    currentSupporters.forEach(supporter => {
+        const newSupporter = document.createElement('p');
+        newSupporter.classList.add('supporter');
+        newSupporter.textContent = supporter;
+        listContainer.appendChild(newSupporter);
+    });
 }
 
 export default renderSupporters;
